9e145b672a4337f98edfa2baa62a10e3cda769ac
Weeraya Watchara-amphaiwan <135736139+vvryw@users.noreply.github.com>
2024-12-15 21:27:54 +0700

    Create 01_​Expr_​15_​An_​Expression_​II.cpp

---
+++ grader/​Expr/01_​Expr_​15_​An_​Expression_​II.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double x, y;
+    cin >> x;
+    y = pow(x, sqrt(log(pow(x+1,2)))) / (10-x);
+    cout << round(y*1e6)/1e6;
+    return 0;
+}
