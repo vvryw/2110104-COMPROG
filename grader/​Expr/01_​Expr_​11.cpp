e76561c651e210f0c86fcf4ecc6b4f89e8596f73
Weeraya Watchara-amphaiwan <135736139+vvryw@users.noreply.github.com>
2024-12-15 21:23:05 +0700

    Create Expr folder

---
+++ grader/​Expr/01_​Expr_​11.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <cmath>
+int main(){
+    cout << (M_PI - 10*9*8*7*6*5*4*3*2/pow(8.0,8.0) + pow(log(9.7), (7.0/sqrt(71.0) - sin(40*M_PI/180.0))))/pow(1.2, pow(2.3,1/3.0));
+}
